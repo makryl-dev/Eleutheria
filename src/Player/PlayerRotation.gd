343a50f87c86c1bfbfe9a54844babe738946eadb
Makryl <dawidminor90@gmail.com>
2026-07-25 00:59:29 +0200

    feat: add PlayerRotation component and attach it to Player

---
+++ src/Player/PlayerRotation.gd
@@ -0,0 +1,14 @@
+class_name PlayerRotation
+extends Node2D
+var parent: CharacterBody2D
+
+func _ready() -> void:
+	parent = get_parent() as CharacterBody2D
+	if not parent:
+		push_error("Component parent has to be CharacterBody2D")
+
+func _physics_process(delta: float) -> void:
+	if not parent:
+		return
+	
+	parent.look_at(get_global_mouse_position())
