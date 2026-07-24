322cd8dfc2ade1662e3c7e239c3c1285f7eb80c2
Makryl <dawidminor90@gmail.com>
2026-07-25 00:41:04 +0200

    feat: add PlayerMovement component and attach it to Player

---
+++ src/Player/PlayerMovement.gd
@@ -0,0 +1,17 @@
+class_name PlayerMovement
+extends Node2D
+@export var speed = 300.0
+var parent: CharacterBody2D
+
+func _ready() -> void:
+	parent = get_parent() as CharacterBody2D
+	if not parent:
+		push_error("Component parent has to be CharacterBody2D")
+
+func _physics_process(delta: float) -> void:
+	if not parent:
+		return
+	
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	parent.velocity = direction * speed
+	parent.move_and_slide()
